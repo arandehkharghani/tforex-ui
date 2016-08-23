91fa6657ceca68f42bb923b63b9f38b6b30c89ee
Aran Dehkharghani <aran.dehkharghani@gmail.com>
2016-08-23 16:18:15 +1000

    fix(build): remove js files from git

--- app/strategy/test.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-    moduleId: module.id,
-    selector: 'sg-test',
-    template: '<h2>hi</h2>',
-})
-export class FeatureComponent {
-}
